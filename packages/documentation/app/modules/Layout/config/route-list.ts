e52630eff8f75bfb56d0bae359d9fe46e90c406d
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2024-03-20 16:10:53 -0300

    feat: add input menu

--- packages/documentation/app/modules/Layout/config/route-list.ts
+++ packages/documentation/app/modules/Layout/config/route-list.ts
@@ -2,6 +2,7 @@ const route_components = [
   { to: "/v1/components/badge", label: "Badge" },
   { to: "/v1/components/button", label: "Button" },
   { to: "/v1/components/icon-button", label: "IconButton" },
+  { to: "/v1/components/input", label: "Input" },
 ];
 
 export { route_components };
