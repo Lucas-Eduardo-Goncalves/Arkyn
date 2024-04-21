bbfcf49e9d4119ef3ead236f2f99c82c80c7abf3
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2024-04-21 11:08:25 -0300

    feat: update a generate slug doc

--- packages/documentation/app/modules/SharedGenerateSlug/components/Use.tsx
+++ packages/documentation/app/modules/SharedGenerateSlug/components/Use.tsx
@@ -18,23 +18,6 @@ function someFunction() {
   return slug; // "i-love-my-cat"
 }`}
       </Fence>
-
-      <Paragraph>She follows a detailed process to ensure slugs are:</Paragraph>
-      <Paragraph>
-        <strong>- Compatible with URLs:</strong> Removes special characters that
-        are not allowed in URLs, such as accents (<code>à</code>, <code>ê</code>
-        , <code>í</code>) and punctuation (<code>!</code>, <code>@</code>,{" "}
-        <code>#</code>). <br />
-        <strong>-Separated by hyphen:</strong> Replaces white spaces with
-        hyphens (<code>-</code>), making the slug more readable and separating
-        the words. <br />
-        <strong>- Lowercase</strong>: Converts the entire string to lowercase,
-        ensuring consistency in slug generation. <br />
-        <strong>- Clean hyphens:</strong> Removes repeated sequences of hyphens,
-        avoiding disorganized URLs. <br />
-        <strong>- No hyphens at the beginning or end:</strong> Remove hyphens at
-        the beginning and at the end of the slug, ensuring valid URLs.
-      </Paragraph>
     </>
   );
 }
