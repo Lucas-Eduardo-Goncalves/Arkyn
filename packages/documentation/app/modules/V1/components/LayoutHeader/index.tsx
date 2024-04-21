82eaa4f1b2bd904fe955ec698d9a29855a5be1de
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2024-04-21 11:25:25 -0300

    fix: correct a elements in layout

--- packages/documentation/app/modules/V1/components/LayoutHeader/index.tsx
+++ packages/documentation/app/modules/V1/components/LayoutHeader/index.tsx
@@ -1,13 +1,18 @@
 import { Link } from "@remix-run/react";
 import { Container } from "./styles";
+import { Tooltip } from "@arkyn/components";
 
 function LayoutHeader() {
   return (
     <Container>
       <h1>Arkyn</h1>
       <nav>
-        <Link to="/v1/contact">Contact</Link>
-        <Link to="/auth/sign-in?redirectURL=/v2/channels">Inbox API</Link>
+        <Tooltip text="Contact the developer" orientation="left">
+          <Link to="/v1/contact">Contact</Link>
+        </Tooltip>
+        <Tooltip text="Access your flow control" orientation="left">
+          <Link to="/auth/sign-in?redirectURL=/v2/channels">Inbox API</Link>
+        </Tooltip>
       </nav>
     </Container>
   );
