4156f75287537c825b4f2bd42395310df2f737f3
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2024-05-10 18:30:27 -0300

    fix: update form error

--- packages/components/src/components/Form/FormError/index.tsx
+++ packages/components/src/components/Form/FormError/index.tsx
@@ -1,11 +1,16 @@
+import { ReactNode } from "react";
 import { useFormController } from "../FormController";
 
 import "./styles.css";
 
-function FormError() {
+type FormErrorProps = {
+  children?: ReactNode;
+};
+
+function FormError({ children }: FormErrorProps) {
   const { error } = useFormController();
   if (!error) return <></>;
-  return <strong className="arkyn_form_error">{error}</strong>;
+  return <strong className="arkyn_form_error">{children || error}</strong>;
 }
 
 export { FormError };
