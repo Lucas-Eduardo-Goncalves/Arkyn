47f28f27a187c62ebaaeb819fc127d6dfdde859b
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2024-05-13 11:34:29 -0300

    feat: add a currency format function in shared package

---
+++ packages/shared/src/currencyFormat.ts
@@ -0,0 +1,10 @@
+function currencyFormat(value: number) {
+  const format = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
+
+  return { value: format, valueWithotPrefix: format.replace("R$", "").trim() };
+}
+
+export { currencyFormat };
