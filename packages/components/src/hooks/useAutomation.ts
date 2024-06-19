c3def343254c95164cd4b62a24099d797ac868f1
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2024-06-19 00:00:25 -0300

    feat: add toasts in useAutomation

--- packages/components/src/hooks/useAutomation.ts
+++ packages/components/src/hooks/useAutomation.ts
@@ -2,14 +2,27 @@ import { useActionData } from "@remix-run/react";
 import { useContext, useEffect } from "react";
 
 import { ModalContext } from "../context/ModalContext";
+import { useToast } from "./useToast";
 
 function useAutomation() {
   const actionData = useActionData<any>();
   const { closeModal } = useContext(ModalContext);
+  const { successToast, errorToast } = useToast();
 
   useEffect(() => {
     const closeModalKey = actionData?.closeModalKey;
     if (closeModalKey) closeModal(closeModalKey);
+  }, [actionData]);
+
+  useEffect(() => {
+    const showToast =
+      typeof actionData?.message === "string" &&
+      typeof actionData?.success === "boolean";
+
+    if (showToast) {
+      if (actionData?.success) successToast(actionData?.message);
+      else errorToast(actionData?.message);
+    }
   }, [actionData]);
 }
 
