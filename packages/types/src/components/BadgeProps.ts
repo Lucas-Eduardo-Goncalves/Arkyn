16b71012531deac22af6b4c41a0f6bd03cd3a013
Lucas Gonçalves <lucasgoncalves@MacBook-Air-de-Lucas.local>
2024-08-01 22:47:56 -0300

    feat: add style support for dark theme in badge component

--- packages/types/src/components/BadgeProps.ts
+++ packages/types/src/components/BadgeProps.ts
@@ -6,7 +6,7 @@ type BaseBadgeProps = HTMLAttributes<HTMLDivElement>;
 type BadgeProps = BaseBadgeProps & {
   size?: "md" | "lg";
   variant?: "solid" | "outline" | "ghost";
-  scheme?: "primary" | "secondary" | "success" | "warning" | "danger" | "info";
+  scheme?: "primary" | "success" | "warning" | "danger" | "info";
 
   leftIcon?: LucideIcon;
   rightIcon?: LucideIcon;
