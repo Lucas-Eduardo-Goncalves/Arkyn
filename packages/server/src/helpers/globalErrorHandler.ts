ab38cf9481a0fe65ca7b5559f6685f75b869e36a
Lucas Eduardo Gonçalves <lucasgoncalvesgithub@gmail.com>
2025-03-05 11:26:16 -0300

    feat(error-handling): add conditional console error logging
    Add a feature to log errors to the console based on environment variables.
    This improves debugging capabilities in development and when explicitly enabled.
    
    - Check if NODE_ENV is "development" or SHOW_ERRORS_IN_CONSOLE is "true"
    - Log error to console if conditions are met

--- packages/server/src/helpers/globalErrorHandler.ts
+++ packages/server/src/helpers/globalErrorHandler.ts
@@ -13,6 +13,12 @@ import {
 } from "../httpBadResponses/unauthorized";
 
 const globalErrorHandler = (error: any) => {
+  const showConsoleError =
+    process.env.NODE_ENV === "development" ||
+    process.env?.SHOW_ERRORS_IN_CONSOLE === "true";
+
+  if (showConsoleError) console.error(error);
+
   if (error instanceof Response) throw error;
 
   switch (true) {
