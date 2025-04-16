6c250157251a461ff8e00cc333f92312e1279156
Lucas Eduardo Gonçalves <lucasgoncalvesgithub@gmail.com>
2025-04-15 22:52:54 -0300

    feat(shared): add new utility functions and reorder exports
    - Add new functions: ensureQuotes, removeCurrencySymbols, stripHtmlTags, validatePassword
    - Reorder formatToDate export in formats section
    - Update exports order for better organization

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -7,10 +7,10 @@ export { formatToCnpj } from "./formats/formatToCnpj";
 export { formatToCpf } from "./formats/formatToCpf";
 export { formatToCpfCnpj } from "./formats/formatToCpfCnpj";
 export { formatToCurrency } from "./formats/formatToCurrency";
+export { formatToDate } from "./formats/formatToDate";
 export { formatToEllipsis } from "./formats/formatToEllipsis";
 export { formatToHiddenDigits } from "./formats/formatToHiddenDigits";
 export { formatToPhone } from "./formats/formatToPhone";
-export { formatToDate } from "./formats/formatToDate";
 
 // generators
 export { generateColorByString } from "./generators/generateColorByString";
@@ -19,8 +19,11 @@ export { generateSlug } from "./generators/generateSlug";
 
 // services
 export { calculateCardInstallment } from "./services/calculateCardInstallment";
+export { ensureQuotes } from "./services/ensureQuotes";
 export { maskSensitiveData } from "./services/maskSensitiveData";
+export { removeCurrencySymbols } from "./services/removeCurrencySymbols";
 export { removeNonNumeric } from "./services/removeNonNumeric";
+export { stripHtmlTags } from "./services/stripHtmlTags";
 export { truncateLargeFields } from "./services/truncateLargeFields";
 
 // utils
@@ -28,5 +31,6 @@ export { validateCep } from "./validations/validateCep";
 export { validateCnpj } from "./validations/validateCnpj";
 export { validateCpf } from "./validations/validateCpf";
 export { validateDate } from "./validations/validateDate";
+export { validatePassword } from "./validations/validatePassword";
 export { validatePhone } from "./validations/validatePhone";
 export { validateRg } from "./validations/validateRg";
