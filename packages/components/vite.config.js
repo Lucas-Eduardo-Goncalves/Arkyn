241337e73dbb181b94bf6a81e85604c0c625d923
Lucas Eduardo Gonçalves <lucasgoncalvesgithub@gmail.com>
2025-05-13 16:05:15 -0300

    chore(deps): upgrade packages to v2.2.11 and add @vitejs/plugin-react
    - Bump @arkyn/components, @arkyn/server, @arkyn/shared, @arkyn/templates, and @arkyn/types from 2.2.10 to 2.2.11
    - Add @vitejs/plugin-react v4.4.1 as a dependency to @arkyn/components
    - Configure Vite to use React plugin in @arkyn/components

--- packages/components/vite.config.js
+++ packages/components/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     minify: true,
     outDir: "./dist",
